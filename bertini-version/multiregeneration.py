5fac7861f8c2e51b8458fe175ad4d3ce75d6f25b
Jose <jose.israel.rodriguez.math@gmail.com>
2019-06-10 17:10:21 -0500

    First round of comments

--- bertini-version/multiregeneration.py
+++ bertini-version/multiregeneration.py
@@ -1,12 +1,12 @@
-#TODO: 1. have a intermediate steps dir where you do all the 
+#TODO: 1. have a intermediate steps dir where you do all the
 # homotopies. Check for it and delete it at the beginning of each run
-# 2. Do the projective case. 3. Think about the hypothesis that would 
+# 2. Do the projective case. 3. Think about the hypothesis that would
 # ensure the following: if a point ...
 
 import shutil
 import sys
 import subprocess
-import hashlib 
+import hashlib
 import os
 
 # variables = [["x1", "x2"], ["y1", "y2"]]
@@ -19,11 +19,11 @@ import os
 # functionNames = ["f1", "f2"]
 # tolerance = 1e-10
 # # The l_i,j  are the dimention linears in the variables i the i'th group
-# l = [["234*x2 - x1 - 234.2341", "x2 + 2*x1 - 1231"], ["19*y2 - y1 - 1123.235", 
+# l = [["234*x2 - x1 - 234.2341", "x2 + 2*x1 - 1231"], ["19*y2 - y1 - 1123.235",
 #     "y2 + 393*y1 - 12"]]
-# # The r_i,d are the regeneration linears in the i'th variable group. 
+# # The r_i,d are the regeneration linears in the i'th variable group.
 # # For fixed i, there are maxDegree[i] - 1
-# r = [[None, "x1 - 2*x2 + 2.1"], [None, "5*y1 + y2 - 25"]] #So that the 
+# r = [[None, "x1 - 2*x2 + 2.1"], [None, "5*y1 + y2 - 25"]] #So that the
 # # indexing matches the write up, we want the r to start at 1.
 
 
@@ -45,6 +45,8 @@ variablesString = ",".join(flatVariablesList)
 
 
 def vanishes(dirName, variablesString, functionString, point):
+# a function that returns True if functionString defines a polynomial vanishing at point
+# What is going on with the comments below?
     # currentVariable = 0
     # evalString = function
     # for z in point.splitlines():
@@ -54,67 +56,63 @@ def vanishes(dirName, variablesString, functionString, point):
     # evalString = evalString.replace("^", "**")
     # return eval(evalString)
 
-
-    inputText = '''
-        CONFIG
-
-        TrackType:-4;
-
-        END;
-
-        INPUT
-
-        variable_group %s;
-        function f;
-
-        f = %s;
-
-        END;
-    ''' % (variablesString, 
-                functionString)
-    startText = "1\n\n"+point
-    try:
+    try:  # What is happening here?
         os.mkdir(dirName)
     except:
         print("Ope! Error opening directory '%s'"%dirName)
-
+# Write input file for evaluation.
+# TODO: replace variable_group %s with a list of variable groups from variables
+# and indicate if hom_variable_group or variable_group
+    inputText = '''
+        CONFIG
+            TrackType:-4;
+        END;
+        INPUT
+            variable_group %s;
+            function f;
+            f = %s;
+        END;
+    ''' % (variablesString,
+                functionString)
     inputFile = open("%s/input"%dirName, "w")
     inputFile.write(inputText)
     inputFile.close()
 
+# Write start file for evaluation.
+# Why don't we copy this file from nonsingular_solutions?
+    startText = "1\n\n"+point
     startFile = open("%s/start"%dirName, "w")
     startFile.write(startText)
     startFile.close()
 
+# Get current working directory to run Bertini.
     cwd = os.getcwd()
     os.chdir(dirName)
-
     try:
         bertiniCommand = "bertini input"
         process = subprocess.Popen(bertiniCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
     except:
-        print(error)
-
+        print(error) # Need a description of this error.
+# Open the Bertini output file named 'function' and determine if this is zero.
     try:
         solutionsFile = open("function", "r")
         solutionsLines = solutionsFile.readlines()
         solutionsFile.close()
     except:
         print("Ope! Error opening file 'function'")
-
-    # TODO return true of false for zero of nonzero
+    # TODO return true of false for zero of nonzero  # isn't this already done?
     value = solutionsLines[2].split(' ')
-
     zerosPastDecimalReal = int(value[0].split('e')[1])
     zerosPastDecimalImaginary = int(value[1].split('e')[1])
 
-    os.chdir(cwd)
-    return (zerosPastDecimalReal < logTolerance 
+    os.chdir(cwd) #Can we move this right after the previous except?
+    return (zerosPastDecimalReal < logTolerance
             and zerosPastDecimalImaginary < logTolerance)
 
-def directoryName(prefix, useFunction, currentDimension, 
-        regenerationLinearIndex, point):
+
+def directoryName(prefix, useFunction, currentDimension, regenerationLinearIndex, point):
+# Makes the directory for each process.
     dirName = prefix
     for b in useFunction:
         if b:
@@ -128,38 +126,32 @@ def directoryName(prefix, useFunction, currentDimension,
     dirName +="_r_%d_%d"%tuple(regenerationLinearIndex)
     dirName +="_hash_%d"%(abs(hash(point)) % (10 ** 8)) # add a hash
     return dirName
-            
 
-    
 
 def homotopy(dirName, variablesString, functionNames, functionsList, indexToTrack, targetFunctionString, startSolutionsList):
+# Now write Bertini-input files and call a homotopy.
     body = ""
     for i in range(len(functionsList)):
         if i is not indexToTrack:
             body+="\n %s = %s;"%(functionNames[i], functionsList[i])
         elif i is indexToTrack:
-            body+="\n %s = s*(%s) + (1-s)*(%s);"%(functionNames[i], 
+            body+="\n %s = s*(%s) + (1-s)*(%s);"%(functionNames[i],
                     functionsList[i], targetFunctionString)
+# TODO: replace variable %s with a list of variable groups from variables
+# and indicate if hom_variable_group or variable_group
     inputText = '''
         CONFIG
-
-        UserHomotopy: 1;
-
+            UserHomotopy: 1;
         END;
-
         INPUT
-
-        variable %s;
-        function %s;
-        pathvariable t;
-        parameter s;
-
+            variable %s;
+            function %s;
+            pathvariable t;
+            parameter s;
         s = t;
-
         %s
-
         END;
-    ''' % (variablesString, 
+    ''' % (variablesString,
                 ",".join(functionNames),body)
     startBody = ""
     for sol in startSolutionsList:
@@ -221,22 +213,22 @@ def regenerate(useFunction, currentDimension, regenerationLinearIndex, point):
             regenerationSystemFunctionNames.append("l_%d_%d"%(i,j))
             currentIndexInSystem += 1
 
-        if i is regenerationLinearIndex[0]: 
-        # If the current variable group is the target one 
-            regenerationSystemTargetIndex = currentIndexInSystem 
-            # Then the target l to get rid of is the last one in that 
+        if i is regenerationLinearIndex[0]:
+        # If the current variable group is the target one
+            regenerationSystemTargetIndex = currentIndexInSystem
+            # Then the target l to get rid of is the last one in that
             # variable group
 
-    dirName = directoryName("regen_", useFunction, currentDimension, 
+    dirName = directoryName("regen_", useFunction, currentDimension,
             regenerationLinearIndex, point)
 
     print("regenerating point = %s"%point)
-    regeneratedPoint = homotopy(dirName, 
-            variablesString, 
+    regeneratedPoint = homotopy(dirName,
+            variablesString,
             regenerationSystemFunctionNames,
             regenerationSystem,
-            regenerationSystemTargetIndex, 
-            r[regenerationLinearIndex[0]][regenerationLinearIndex[1]], 
+            regenerationSystemTargetIndex,
+            r[regenerationLinearIndex[0]][regenerationLinearIndex[1]],
             [point])
 
     return regeneratedPoint
@@ -246,23 +238,23 @@ def regenerate(useFunction, currentDimension, regenerationLinearIndex, point):
 # 0,1,0,1
 
 #NOTE: You can keep track of the codimension in each factor, and give up
-# on a solution if you can tell that the codimension will already be too 
+# on a solution if you can tell that the codimension will already be too
 # small.
 
 def regenerateAndTrack(depth, useFunction, currentDimension, regenerationLinearIndex, point):
     if depth >= len(functions):
         return
 
-    checkVanishesDirName = directoryName("evaluate_%d_"%depth, useFunction, currentDimension, 
+    checkVanishesDirName = directoryName("evaluate_%d_"%depth, useFunction, currentDimension,
             regenerationLinearIndex, point)
 
-    if vanishes(checkVanishesDirName, variablesString, 
+    if vanishes(checkVanishesDirName, variablesString,
             functions[depth],
             point):
-        regenerateAndTrack(depth + 1, useFunction, currentDimension, 
+        regenerateAndTrack(depth + 1, useFunction, currentDimension,
                 regenerationLinearIndex, point)
         return
-    
+
     if regenerationLinearIndex[1] is not 0:
         regeneratedPoint = regenerate(useFunction, currentDimension, regenerationLinearIndex, point)
     else:
@@ -271,7 +263,7 @@ def regenerateAndTrack(depth, useFunction, currentDimension, regenerationLinearI
     # TODO: if regenerated point is singular, throw it out
 
     #track the regenerated point to the target function
-    dirName = directoryName("track_depth_%d_"%depth, useFunction, currentDimension, 
+    dirName = directoryName("track_depth_%d_"%depth, useFunction, currentDimension,
             regenerationLinearIndex, point)
 
     i = regenerationLinearIndex[0]
@@ -288,24 +280,24 @@ def regenerateAndTrack(depth, useFunction, currentDimension, regenerationLinearI
             linearProductHomotopySytem.append(functions[i])
             linearProductHomotopySytemNames.append(functionNames[i])
     for i in range(len(variables)):
-        for j in range(currentDimension[i]): #exclude the last one 
+        for j in range(currentDimension[i]): #exclude the last one
             # because we are using the linear product instead
-            if not (i is regenerationLinearIndex[0] and j is 
+            if not (i is regenerationLinearIndex[0] and j is
                     currentDimension[i]-1):
                 linearProductHomotopySytem.append(l[i][j])
                 linearProductHomotopySytemNames.append("l_%d_%d"%(i,j))
 
     print("tracking point = %s"%point)
-    trackedPoint = homotopy(dirName, 
-            variablesString, 
+    trackedPoint = homotopy(dirName,
+            variablesString,
             linearProductHomotopySytemNames,
             linearProductHomotopySytem,
-            0, 
-            functions[depth], 
+            0,
+            functions[depth],
             [regeneratedPoint])
 
     for i in range(len(variables)):
-        if currentDimension[i] is not 0: #TODO: replace this with a condition 
+        if currentDimension[i] is not 0: #TODO: replace this with a condition
             # that depends on the expexcted multidimension
             if depth+1 < len(functions):
                 for j in range(degrees[depth+1][i]):
@@ -337,10 +329,10 @@ def main():
 
     for i in range(len(variables)):
         for j in range(len(variables[i])):
-            regenerateAndTrack(0, [False for f in functions], 
+            regenerateAndTrack(0, [False for f in functions],
                     [len(group) for group in variables], [i,j], startSolution)
 
     os.chdir("..")
-  
+
 if __name__== "__main__":
   main()
