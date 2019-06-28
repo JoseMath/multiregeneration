3311100c2afc4160173ac23843c257851b9eed6e
Colin <cwcrowley@wisc.edu>
2019-06-28 09:08:58 -0500

    kinimatics examples

---
+++ bertini-version/test-3pose/inputFile.py
@@ -0,0 +1,7 @@
+maxProcesses = 3
+degrees = [[2,2,0,0],[2,2,0,0],[0,0,2,2],[0,0,2,2]]
+logTolerance = -10
+workingDirectory = "run"
+depth = 0
+bfe=[2,2,2,2]
+B=4
---
+++ bertini-version/test-5pose/inputFile.py
@@ -0,0 +1,7 @@
+verbose = 2
+maxProcesses = 3
+degrees = [[2,2,0,0],[2,2,0,0],[2,2,0,0],[2,2,0,0],[0,0,2,2],[0,0,2,2],[0,0,2,2],[0,0,2,2]]
+logTolerance = -10
+targetDimensions = [[0,0,0,0]]
+workingDirectory = "run"
+depth = 0
