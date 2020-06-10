4d3555642b0bb52628fc9ae4b81e8df1d4161716
Jose <jose@math.wisc.edu>
2020-06-10 11:31:49 -0500

    pathToBertini commented out for ease

--- CyclicRoots/inputFile.py
+++ CyclicRoots/inputFile.py
@@ -6,3 +6,6 @@ verbose = 1
 
 #The maximum number of processes to use
 maxProcesses = 3
+
+#Uncomment and change to specify a different path to Bertini
+# pathToBertini = "/Users/Alice/Desktop/BertiniApple32_v1.5.1/bertini"
